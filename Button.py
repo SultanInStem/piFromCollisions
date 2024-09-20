466a06f05a92dad0ddcad21191d2dcb5f2e5b845
SultanInStem <sultaninstem004@gmail.com>
2024-09-20 16:55:05 -0700

    button update

--- Button.py
+++ Button.py
@@ -1,10 +1,14 @@
 import pygame
 class Button: 
-    def __init__(self,x,y,width,height,title):
-        self.x = x 
-        self.y = y 
-        self.w = width 
-        self.h = height
+    font = pygame.font.Font(None, 24)
+    def __init__(self, title, pos, size):
+        self.x = pos[0] 
+        self.y = pos[1] 
+        self.w = size[0]
+        self.h = size[1]
         self.title = title
-    def listenForInput(self, pos): ## pass pos as a tuple (x,y)
+    def listenForInput(self, pos): 
+        ## pass pos of the mouse as a tuple (x,y)
+        return (self.x < pos[0] < self.x + self.w) and (self.y < pos[1] < self.y + self.h)
+    def show(self, screen): 
         pass
