faa77ff3496a135afe343f30eec1e525d97bc308
= <sultaninstem004@gmail.com>
2024-09-22 13:22:56 -0700

    centered labels

--- utils/Block.py
+++ utils/Block.py
@@ -9,13 +9,16 @@ class Block:
         self.default_size = size
         self.rect = pygame.Rect(self.default_pos, size)
         self.font = pygame.font.Font(None, 36)
-        self.label = self.font.render("m1", True, (255,245,255))
+        self.label = self.font.render(f"m{id}", True, (255,245,255))
     def show(self, screen):
         x = self.rect.x 
         y = self.rect.y 
-        size = self.rect.size
+        rect_size = self.rect.size
+        text_width = self.label.get_size()[0]
+        text_height = self.label.get_size()[1]
+        text_pos = (x + (rect_size[0] - text_width) // 2, y + (rect_size[1] - text_height) // 2)
         pygame.draw.rect(screen, (0,0,0), self.rect)
-        screen.blit(self.label, (x + size[0] // 2, y + size[1] // 2))
+        screen.blit(self.label, text_pos)
     def move(self):
         self.rect.x += self.vi
     def reset_pos(self): 
